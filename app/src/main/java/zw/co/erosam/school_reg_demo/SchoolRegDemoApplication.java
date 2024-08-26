05aa33fe6adf7ac02c2817e5589a004e990b88e4
tmasore <tmasore@gmail.com>
2024-08-26 19:22:12 +0200

    project setup

---
+++ app/src/main/java/zw/co/erosam/school_reg_demo/SchoolRegDemoApplication.java
@@ -0,0 +1,13 @@
+package zw.co.erosam.school_reg_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolRegDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolRegDemoApplication.class, args);
+	}
+
+}
